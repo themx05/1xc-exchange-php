68ff3f23e682ac04ce2edfd93803b58997980fd8
themx05 <maximiliencomlan05@gmail.com>
2020-08-01 01:55:37 +0100

    Quick fix on file router to support icons url

--- routes/files.php
+++ routes/files.php
@@ -6,7 +6,20 @@ use Routing\Router;
 
 $fileRouter = new Router();
 
-$fileRouter->get("/:file", function(Request $req, Response $res){
+$fileRouter->get("/files/:file", function(Request $req, Response $res){
+    $filename = $req->getParam("file");
+    $homeDir= $req->getOption('home');
+    $uploadDir = "$homeDir/uploads";
+    $filePath = "$uploadDir/$filename";
+    if(file_exists($filePath) && is_file($filePath)){
+        $res->file($filePath);
+    }
+    else{
+        $res->status(404)->text("file not found");
+    }
+});
+
+$fileRouter->get("/icons/:file", function(Request $req, Response $res){
     $filename = $req->getParam("file");
     $homeDir= $req->getOption('home');
     $uploadDir = "$homeDir/uploads";
@@ -20,5 +33,5 @@ $fileRouter->get("/:file", function(Request $req, Response $res){
 });
 
 global $application;
-$application->router("/files", $fileRouter);
+$application->router("/",$fileRouter);
 ?>
