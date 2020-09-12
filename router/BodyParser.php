6f6c6970562d26f79ae7863c812a3e7516a24eea
themx05 <maximiliencomlan05@gmail.com>
2020-09-12 21:26:30 +0100

    updated logic in bodyparser

--- router/BodyParser.php
+++ router/BodyParser.php
@@ -9,9 +9,11 @@ namespace Routing{
         public static function json(): Closure{
             return function(Request& $request, Response $response, Closure $next){
                 $match = array();
-                if(preg_match("/^application\/json/i",$request->headers['Content-Type'], $match)){
-                    $input = json_decode(file_get_contents("php://input"));
-                    $request->setOption('body',$input);
+                if(isset($request->headers['Content-Type'])){
+                    if(preg_match("/^application\/json/i",$request->headers['Content-Type'], $match)){
+                        $input = json_decode(file_get_contents("php://input"));
+                        $request->setOption('body',$input);
+                    }
                 }
                 $next();
             };
