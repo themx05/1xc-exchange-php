58869e90960d77611743c8db17d4d44ddd40a7e3
themx05 <maximiliencomlan05@gmail.com>
2020-09-23 09:24:13 +0100

    Quick Fix

--- Routing/BodyParser.php
+++ Routing/BodyParser.php
@@ -9,7 +9,7 @@ namespace Routing{
         public static function json(): Closure{
             return function(Request& $request, Response $response, Closure $next){
                 $match = array();
-                if(preg_match("/^application\/json/i",$request->headers['content-type'], $match)){
+                if(isset($request->headers['content-type']) && preg_match("/^application\/json/i",$request->headers['content-type'], $match)){
                     $input = json_decode(file_get_contents("php://input"));
                     $request->setOption('body',$input);
                 }
